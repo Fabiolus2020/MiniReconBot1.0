094ebcfdc1473b0163e200f3b8a24e5170e213ea
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-03-10 00:05:27 -0500

    Update controller.ino

--- controller.ino
+++ controller.ino
@@ -1,6 +1,6 @@
-//Fabiolus 2021/01/15
-//the_fabiolous@otmail.com
-
+//Fabiolus 2021/02/23
+//the_fabiolous@hotmail.com
+#include <ezButton.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -9,16 +9,18 @@ RF24 myRadio(8, 9); // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-
+//data being sent to the receiver, joystick position, pot values and button count.
 struct package
 {
   int joyposX;
   int joyposY;
   int potValue1;
   int potValue2;
-  int modeNow;
+  int modeCount;
 };
 
+
+//define object for wieless communication
 typedef struct package Package;
 Package data;
 
@@ -27,20 +29,32 @@ Package data;
 #define joyY A1
 #define readPot1 A2
 #define readPot2 A3
-#define buttonPin 7
 #define ledModeStatusCount 6
 
-// Define Joystick Values - Start at 512 (middle position)
+// Define Joystick Values - Start at 512 (middle position) this might change depending on batteries and motor type.
 int joyposX = 512;
 int joyposY = 512;
 int potValue1 = 0;
 int potValue2 = 0;
-int buttonState      = 0;     // current state of the button
-int lastButtonState  = 0;     // previous state of the button
-int modeNow;
+
+
+//initial value of animation1 and 2 //animation1 is to control counter to cycle throught Mode1(frontlight,backlight,left,rightlight)
+//animation2 is a counter to cycle through the ALA predefined templates, 7 in totals that I chose based on current, the lowest the better.
+int animation1Count = 0;
+int animation2Count = 0;
+int buttonJoystickCount = 0;
+int modeCount;
+
+//defining buttons using ezbutton library
+ezButton modeButton(7);  // create ezButton object that attach to pin 7;
+ezButton buttonJoystick(5);  // create ezButton object that attach to pin 5;
+ezButton buttonForward(4);  // create ezButton object that attach to pin 4;
+ezButton buttonBackward(3);  // create ezButton object that attach to pin 3;
 
 void setup() {
   Serial.begin(9600);
+
+  //define Radio as object
   myRadio.begin();
 
   //set the address
@@ -54,53 +68,51 @@ void setup() {
   pinMode(ledModeStatusCount, OUTPUT);
   digitalWrite(ledModeStatusCount, LOW);
   delay(10);
-  pinMode(buttonPin, INPUT_PULLUP);  // initialize the button pin as a input
+
+
+  //The below I use for debounce issue using the EZbutton library, had to adjust for the switch button on thumbjoystick
+  modeButton.setDebounceTime(50); // set debounce time to 50 milliseconds
+  buttonJoystick.setDebounceTime(80); // set debounce time to 80 milliseconds
+  buttonForward.setDebounceTime(50); // set debounce time to 50 milliseconds
+  buttonBackward.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 void loop() {
+
+  modeButton.loop(); // MUST call the loop() function first
+  buttonJoystick.loop(); // MUST call the loop() function first
+  buttonForward.loop(); // MUST call the loop() function first
+  buttonBackward.loop(); // MUST call the loop() function first
+
+  //from EZbutton library to keep count of buttons count. Works great!
+  unsigned long modeButtonCount = modeButton.getCount();
+  unsigned long buttonJoystickCount = buttonJoystick.getCount();
+  unsigned long buttonForwardCount = buttonForward.getCount();
+  unsigned long buttonBackwardCount = buttonBackward.getCount();
+
+  //prepare to capture data to pass wirelessly using data for identifier.
   myRadio.write(&data, sizeof(data));
 
   data.joyposX = analogRead(joyX);
   data.joyposY = analogRead(joyY);
   data.potValue1 = analogRead(readPot1);
   data.potValue2 = analogRead(readPot2);
-  buttonState = digitalRead(buttonPin);
 
-  //Serial.print("xAxis :");
-  Serial.println(data.joyposX);
-  //Serial.print("yAxis :");
-  Serial.println(data.joyposY);
-  // Serial.print("PotValue1 :");
- //  Serial.println(data.potValue1);
-  // Serial.print("PotValue2 :");
-  // Serial.println(data.potValue2);
-  //Serial.print("ModeNow :");
-  //Serial.println(data.modeNow);
-  delay(10);
+//Serial.println(data.joyposX);
+Serial.println(data.joyposY);
+  //for some reason released works better then IsPressed
+  if (modeButton.isReleased()) {
 
-  // check if the button is pressed or released
-  // by comparing the buttonState to its previous state
-  if (buttonState != lastButtonState) {
+    if (modeButtonCount >= 4) modeButton.resetCount();
 
-    // change the state of the led when someone pressed the button
-    if (buttonState == 1) {
-      data.modeNow++;
-      if (data.modeNow > 4) data.modeNow = 1;
-      for (int i = 0; i < data.modeNow; i++) {
-        digitalWrite(ledModeStatusCount, LOW);
-        delay(400);
-        digitalWrite(ledModeStatusCount, HIGH);
-        delay(400);
-        digitalWrite(ledModeStatusCount, LOW);
 
-      }
+    for (int i = 0; i < modeButtonCount; i++) {
+      digitalWrite(ledModeStatusCount, LOW);
+      delay(400);
+      digitalWrite(ledModeStatusCount, HIGH);
+      delay(400);
+      digitalWrite(ledModeStatusCount, LOW);
     }
-
-    // remember the current state of the button
-    lastButtonState = buttonState;
+    data.modeCount = modeButtonCount;
   }
-
-  // adding a small delay prevents reading the buttonState to fast
-  // ( debouncing )
-  delay(20);
 
 }
