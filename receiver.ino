4d47b5d161a56183fab902f400fe54286f5021c6
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-03-09 23:44:07 -0500

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,8 +1,5 @@
-//Fabiolus
-//the_fabiolous@hotmail.com
-//2020/09/28
-
-// Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+//Fabiolus 2021/02/23
+//the_fabiolous@otmail.com
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -12,14 +9,17 @@ RF24 myRadio(8, 9); // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-
 struct package
 {
   int joyposX;
   int joyposY;
   int potValue1;
   int potValue2;
-  int modeNow;
+  int modeCount;
+  int buttonJoystickCount;
+  int buttonForwardCount;
+  int animation1Count;
+  int animation2Count;
 };
 
 typedef struct package Package;
@@ -33,65 +33,16 @@ Package data;
 #define slp  4  // DRV8833 sleep/enable
 #define flt  2   // overcurrent/overheat fault
 
-// Define Joystick Connections
-//#define joyX A0
-//#define joyY A1
-
-//int joyposX = 512;
-//int joyposY = 512;
-
+// Set initial motor direction as forward and speed at 0
+//int motorSpeedA = 0;
+//int motorSpeedB = 0;
+//int motordirection = 0;
 int MotorSpeed1 = 0;
 int MotorSpeed2 = 0;
 
-// Speed control potentiometers
-//int SpeedControl = A2;
-
-
-void setup() {
-
-  while (!Serial);
-
-  Serial.begin(9600);
-  myRadio.begin();
-  //set the address
-  myRadio.openReadingPipe(0, address);
-  myRadio.setChannel(115);
-  myRadio.setPALevel(RF24_PA_MAX);
-  myRadio.setDataRate( RF24_250KBPS );
-  //Set module as receiver
-  myRadio.startListening();
-
-  pinMode(ain1, OUTPUT);
-  pinMode(ain2, OUTPUT);
-  pinMode(bin1, OUTPUT);
-  pinMode(bin2, OUTPUT);
-  pinMode(slp, OUTPUT);
-  pinMode(flt, INPUT_PULLUP);
-  digitalWrite(ain1, LOW);
-  digitalWrite(ain2, LOW);
-  digitalWrite(bin1, LOW);
-  digitalWrite(bin2, LOW);
-}
-
-void loop() {
-
-  if ( myRadio.available())
-  {
-    myRadio.read( &data, sizeof(data) );
-    // put your main code here, to run repeatedly:
-    //  joyposX = analogRead(joyX);
-    //  joyposY = analogRead(joyY);
-    //  MotorSpeed1 = analogRead(SpeedControl);
-    // MotorSpeed2 = analogRead(SpeedControl);
-
-
-    Serial.println(data.joyposX);
-   // Serial.println(data.joyposY);
-   // Serial.println(data.potValue1);
-    //Serial.println(joyposY);
-
-    // check for a fault
-    if (digitalRead(flt) != LOW) {
+void modeOne()
+{
+  if (digitalRead(flt) != LOW) {
 
 
 
@@ -172,5 +123,78 @@ void loop() {
     //}
 
     delay(20);
+
+}
+
+
+void modeTwo()
+{
+
+}
+
+void modeThree()
+{
+
+
+}
+
+void modeFour()
+{
+  
+}
+
+void setup() {
+
+  while (!Serial);
+
+  Serial.begin(9600);
+  myRadio.begin();
+  //set the address
+  myRadio.openReadingPipe(0, address);
+  myRadio.setChannel(115);
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS );
+  //Set module as receiver
+  myRadio.startListening();
+  
+  pinMode(ain1, OUTPUT);
+  pinMode(ain2, OUTPUT);
+  pinMode(bin1, OUTPUT);
+  pinMode(bin2, OUTPUT);
+  pinMode(slp, OUTPUT);
+  pinMode(flt, INPUT_PULLUP);
+  digitalWrite(ain1, LOW);
+  digitalWrite(ain2, LOW);
+  digitalWrite(bin1, LOW);
+  digitalWrite(bin2, LOW);
+
+}
+
+void loop() {
+
+  if ( myRadio.available())
+  {
+
+    myRadio.read( &data, sizeof(data) );
+
+    if (data.modeCount == 1)
+    {
+
+      modeOne();
+    }
+    if (data.modeCount == 2)
+    {
+      modeTwo();
+    }
+    if (data.modeCount == 3)
+    {
+      modeThree();
+    }
+    if (data.modeCount == 4)
+    {
+      modeFour();
+    }
+
   }
+
 }
